c5284706044958072e9a54dd7b30b2f8a13e7ed9
Yuri Luttsev <floomyz@vk.com>
2025-03-25 17:08:48 +0300

    Обновлена сигнатура и запрос метода DiscountRepository#findByClientCategoryAndProductCategory

--- src/main/java/ru/example/discount/repository/DiscountRepository.java
+++ src/main/java/ru/example/discount/repository/DiscountRepository.java
@@ -6,12 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.query.Param;
 import ru.example.discount.entity.Discount;
+import ru.example.discount.entity.enums.DiscountType;
 
 public interface DiscountRepository extends ListCrudRepository<Discount, Long> {
 
     @Query("from Discount d " +
             "where (d.productCategory.id = :product_category_id or d.productCategory is null) " +
             "and (d.clientCategory.id = :client_category_id or d.clientCategory is null) " +
+            "and d.discountType = :discount_type " +
             "order by " +
             "case " +
             "   when d.productCategory is not null and d.clientCategory is not null then 1 " +
@@ -19,7 +21,8 @@ public interface DiscountRepository extends ListCrudRepository<Discount, Long> {
             "   when d.productCategory is null and d.clientCategory is not null then 3 " +
             "   else 4 " +
             "end")
-    Page<Discount> findAllDiscountsForClientCategoryAndProductCategory(@Param("client_category_id") Long clientCategoryId,
-                                                                       @Param("product_category_id") Long productCategoryId,
-                                                                       Pageable pageable);
+    Page<Discount> findByClientCategoryAndProductCategory(@Param("client_category_id") Long clientCategoryId,
+                                                          @Param("product_category_id") Long productCategoryId,
+                                                          @Param("discount_type") DiscountType discountType,
+                                                          Pageable pageable);
 }
