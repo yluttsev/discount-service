eceb1e8334e859876c7b9e83a1151a0646d66ec7
Yuri Luttsev <floomyz@vk.com>
2025-03-25 18:16:58 +0300

    Добавлен DiscountController

---
+++ src/main/java/ru/example/discount/controller/DiscountController.java
@@ -0,0 +1,26 @@
+package ru.example.discount.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.example.discount.service.DiscountService;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/discount")
+@RequiredArgsConstructor
+public class DiscountController {
+
+    private final DiscountService fixedDiscountService;
+
+    @GetMapping("/fixed")
+    public ResponseEntity<BigDecimal> calculateFixedDiscount(@RequestParam("price") BigDecimal price,
+                                                             @RequestParam("product_category") long productCategoryId,
+                                                             @RequestParam("client_category") long clientCategoryId) {
+        return ResponseEntity.ok().body(fixedDiscountService.calculateDiscount(price, productCategoryId, clientCategoryId));
+    }
+}
