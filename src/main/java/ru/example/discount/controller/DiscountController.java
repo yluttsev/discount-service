615381c0498cf5082beed6bb4060012fb8e33330
Yuri Luttsev <floomyz@vk.com>
2025-03-26 13:49:23 +0300

    Добавлен обработчик ошибок валидации в DiscountController

--- src/main/java/ru/example/discount/controller/DiscountController.java
+++ src/main/java/ru/example/discount/controller/DiscountController.java
@@ -1,7 +1,12 @@
 package ru.example.discount.controller;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,5 +27,17 @@ public class DiscountController {
                                                              @RequestParam("product_category") long productCategoryId,
                                                              @RequestParam("client_category") long clientCategoryId) {
         return ResponseEntity.ok().body(fixedDiscountService.calculateDiscount(price, productCategoryId, clientCategoryId));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ProblemDetail validationException(ConstraintViolationException e) {
+        return ProblemDetail.forStatusAndDetail(
+                HttpStatus.BAD_REQUEST,
+                e.getConstraintViolations()
+                        .stream()
+                        .findFirst()
+                        .map(ConstraintViolation::getMessage)
+                        .orElse("Ошибка валидации")
+        );
     }
 }
