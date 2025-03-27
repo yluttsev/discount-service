55266eef6425feacea947fd21d7292d49d0f1b0b
Yuri Luttsev <floomyz@vk.com>
2025-03-27 18:29:29 +0300

    Исправлены неработающие тесты

--- src/test/java/ru/example/discount/service/impl/FixedDiscountServiceTest.java
+++ src/test/java/ru/example/discount/service/impl/FixedDiscountServiceTest.java
@@ -1,42 +1,48 @@
 package ru.example.discount.service.impl;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.util.ReflectionTestUtils;
 import ru.example.discount.entity.ClientCategory;
 import ru.example.discount.entity.Discount;
 import ru.example.discount.entity.ProductCategory;
 import ru.example.discount.entity.enums.DiscountType;
 import ru.example.discount.repository.DiscountRepository;
-import ru.example.discount.service.DiscountService;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class FixedDiscountServiceTest {
 
-    @MockitoBean
+    @Mock
     private DiscountRepository discountRepository;
 
-    @MockitoBean
-    private DiscountService discountService;
+    @InjectMocks
+    private FixedDiscountService fixedDiscountService;
 
     @Test
     void calculateDiscount_shouldReturnOriginalPriceWhenNoDiscounts() {
+        ReflectionTestUtils.setField(fixedDiscountService, "isDiscountAggregate", false);
+
         when(discountRepository.findByClientCategoryAndProductCategory(
                 anyLong(), anyLong(), any(), any(Pageable.class)
         )).thenReturn(new PageImpl<>(List.of()));
 
-        BigDecimal result = discountService.calculateDiscount(
+        BigDecimal result = fixedDiscountService.calculateDiscount(
                 BigDecimal.valueOf(100), 1L, 1L);
 
         assertEquals(BigDecimal.valueOf(100), result);
@@ -44,34 +50,35 @@ class FixedDiscountServiceTest {
 
     @Test
     void calculateDiscount_shouldApplySingleDiscount() {
-        Discount discount = createDiscount((short)10, (short)10);
+        ReflectionTestUtils.setField(fixedDiscountService, "isDiscountAggregate", false);
+
+        Discount discount = createDiscount((short) 10, (short) 10);
         when(discountRepository.findByClientCategoryAndProductCategory(
                 anyLong(), anyLong(), any(), eq(PageRequest.of(0, 1))
         )).thenReturn(new PageImpl<>(List.of(discount)));
 
-        BigDecimal result = discountService.calculateDiscount(
+        BigDecimal result = fixedDiscountService.calculateDiscount(
                 BigDecimal.valueOf(100), 1L, 1L);
 
-        assertEquals(BigDecimal.valueOf(90), result);
+        assertEquals(BigDecimal.valueOf(90.0), result);
     }
 
     @Test
     void calculateDiscount_shouldAggregateDiscountsWhenAggregationEnabled() {
+        ReflectionTestUtils.setField(fixedDiscountService, "isDiscountAggregate", true);
 
-        ReflectionTestUtils.setField(discountService, "discount.aggregation", true);
-
-        Discount discount1 = createDiscount((short)5, (short)5);
-        Discount discount2 = createDiscount((short)10, (short)10);
+        Discount discount1 = createDiscount((short) 5, (short) 5);
+        Discount discount2 = createDiscount((short) 10, (short) 10);
 
         when(discountRepository.findByClientCategoryAndProductCategory(
-                anyLong(), anyLong(), any(), eq(PageRequest.of(0, 1))
+                anyLong(), anyLong(), any(DiscountType.class), eq(Pageable.unpaged())
         )).thenReturn(new PageImpl<>(List.of(discount1, discount2)));
 
-        BigDecimal result = discountService.calculateDiscount(
+        BigDecimal result = fixedDiscountService.calculateDiscount(
                 BigDecimal.valueOf(100), 1L, 1L
         );
 
-        assertEquals(BigDecimal.valueOf(85), result);
+        assertEquals(BigDecimal.valueOf(85.5).setScale(2, RoundingMode.HALF_UP), result);
     }
 
 
--- src/test/java/ru/example/discount/service/impl/VariableDiscountServiceTest.java
+++ src/test/java/ru/example/discount/service/impl/VariableDiscountServiceTest.java
@@ -2,43 +2,47 @@ package ru.example.discount.service.impl;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.util.ReflectionTestUtils;
 import ru.example.discount.entity.ClientCategory;
 import ru.example.discount.entity.Discount;
 import ru.example.discount.entity.ProductCategory;
 import ru.example.discount.entity.enums.DiscountType;
 import ru.example.discount.repository.DiscountRepository;
-import ru.example.discount.service.DiscountService;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class VariableDiscountServiceTest {
-    @MockitoBean
+class VariableDiscountServiceTest {
+
+    @Mock
     private DiscountRepository discountRepository;
 
-    @MockitoBean
-    private DiscountService discountService;
+    @InjectMocks
+    private VariableDiscountService variableDiscountService;
 
     @Test
     void calculateDiscount_shouldReturnOriginalPriceWhenNoDiscounts() {
+        ReflectionTestUtils.setField(variableDiscountService, "isDiscountAggregate", false);
+
         when(discountRepository.findByClientCategoryAndProductCategory(
                 anyLong(), anyLong(), any(), any(Pageable.class)
         )).thenReturn(new PageImpl<>(List.of()));
 
-        BigDecimal result = discountService.calculateDiscount(
+        BigDecimal result = variableDiscountService.calculateDiscount(
                 BigDecimal.valueOf(100), 1L, 1L);
 
         assertEquals(BigDecimal.valueOf(100), result);
@@ -46,7 +50,9 @@ public class VariableDiscountServiceTest {
 
     @Test
     void calculateDiscount_shouldApplySingleDiscount() {
-        Discount discount = createDiscount((short)5, (short)15);
+        ReflectionTestUtils.setField(variableDiscountService, "isDiscountAggregate", false);
+
+        Discount discount = createDiscount((short) 5, (short) 15);
 
         BigDecimal minExpected = BigDecimal.valueOf(85);
         BigDecimal maxExpected = BigDecimal.valueOf(95);
@@ -55,7 +61,7 @@ public class VariableDiscountServiceTest {
                 anyLong(), anyLong(), any(), eq(PageRequest.of(0, 1))
         )).thenReturn(new PageImpl<>(List.of(discount)));
 
-        BigDecimal result = discountService.calculateDiscount(
+        BigDecimal result = variableDiscountService.calculateDiscount(
                 BigDecimal.valueOf(100), 1L, 1L);
 
         assertTrue(result.compareTo(minExpected) >= 0,
@@ -66,20 +72,19 @@ public class VariableDiscountServiceTest {
 
     @Test
     void calculateDiscount_shouldAggregateDiscountsWhenAggregationEnabled() {
-
-        ReflectionTestUtils.setField(discountService, "discount.aggregation", true);
+        ReflectionTestUtils.setField(variableDiscountService, "isDiscountAggregate", true);
 
         BigDecimal minExpected = BigDecimal.valueOf(60);
         BigDecimal maxExpected = BigDecimal.valueOf(80);
 
-        Discount discount1 = createDiscount((short)10, (short)20);
-        Discount discount2 = createDiscount((short)10, (short)20);
+        Discount discount1 = createDiscount((short) 10, (short) 20);
+        Discount discount2 = createDiscount((short) 10, (short) 20);
 
         when(discountRepository.findByClientCategoryAndProductCategory(
-                anyLong(), anyLong(), any(), eq(PageRequest.of(0, 1))
+                anyLong(), anyLong(), any(DiscountType.class), eq(Pageable.unpaged())
         )).thenReturn(new PageImpl<>(List.of(discount1, discount2)));
 
-        BigDecimal result = discountService.calculateDiscount(
+        BigDecimal result = variableDiscountService.calculateDiscount(
                 BigDecimal.valueOf(100), 1L, 1L
         );
 
