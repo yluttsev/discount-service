3adba9317201020b0c3fc7d54583b4c632e430ba
Yuri Luttsev <floomyz@vk.com>
2025-04-10 15:55:56 +0300

    Добавлен feign client UserServiceClient

---
+++ src/main/java/ru/example/discount/feign/UserServiceClient.java
@@ -0,0 +1,13 @@
+package ru.example.discount.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import ru.example.discount.dto.UserTotalSpentDto;
+
+@FeignClient(name = "user-service-client", url = "${user-service.url}")
+public interface UserServiceClient {
+
+    @GetMapping("/users/{id}")
+    UserTotalSpentDto getUserTotalSpent(@PathVariable("id") Long userId);
+}
