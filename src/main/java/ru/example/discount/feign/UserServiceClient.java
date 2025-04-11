54e6def71003072f063f2a888a469eb3258bbfea
Yuri Luttsev <floomyz@vk.com>
2025-04-11 11:39:02 +0300

    Поправлен docker-compose.yaml

--- src/main/java/ru/example/discount/feign/UserServiceClient.java
+++ src/main/java/ru/example/discount/feign/UserServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.example.discount.dto.UserTotalSpentDto;
 
-@FeignClient(name = "user-service-client", url = "${user-service.url}")
+@FeignClient(name = "user-service-client", url = "${user.service.url}")
 public interface UserServiceClient {
 
     @GetMapping("/users/{id}")
